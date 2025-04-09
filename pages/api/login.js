852d5fcfd14e40167beed83c511785b5e001520e
Patrick Ethier <patrick@secureops.com>
2025-04-09 11:58:12 -0400

    Attempt to fix the CEF format

--- pages/api/login.js
+++ pages/api/login.js
@@ -2,6 +2,8 @@ import db from '../../lib/db.js';
 import bcrypt from 'bcrypt';
 import { generateToken } from './auth.js';
 
+const cef_base='CEF:0|SecureOps|DemoAppTarget|1|0'
+
 export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).end();
 
@@ -10,7 +12,7 @@ export default async function handler(req, res) {
   
   const user = db.prepare('SELECT * FROM users WHERE username = ?').get(username);
   if (!user) {
-    console.log(`action=auth,state=FAIL,username=${username},IP=${ip},reason=NOEXIST`)
+    console.log(`${cef_base}|auth|warn|state=FAIL|username=${username}|IP=${ip}|reason=NOEXIST`)
     return res.writeHead(302, {
       Location: '/unauthorized?error=' + encodeURIComponent('Invalid username or password'),
     }).end();
@@ -18,7 +20,7 @@ export default async function handler(req, res) {
 
   const match = await bcrypt.compare(password, user.password);
   if (!match) {
-    console.log(`action=auth,state=FAILED,username=${username},IP=${ip},reason=BADPASS`)
+    console.log(`${cef_base}|auth|warn|state=FAILED|username=${username}|IP=${ip},reason=BADPASS`)
     return res.writeHead(302, {
       Location: '/unauthorized?error=' + encodeURIComponent('Invalid username or password'),
     }).end();
@@ -27,7 +29,7 @@ export default async function handler(req, res) {
   const token = generateToken(user);
 
   res.setHeader('Set-Cookie', `token=${token}; HttpOnly; Path=/`);
-  console.log(`action=auth,state=SUCCESS,username=${username},IP=${ip},token=${token}`)
+  console.log(`${cef_base}|auth|info|state=SUCCESS|username=${username}|IP=${ip}|token=${token}`)
   res.writeHead(302, { Location: '/profile' });
   res.end();
 }
