7d1bd860d725d927b52c6e486cf45b0a363a9a92
Patrick Ethier <patrick@secureops.com>
2025-04-18 12:47:32 -0400

    First attempt at an attack simulation

---
+++ scripts/attacksession.js
@@ -0,0 +1,72 @@
+const baseUrl = "https://demotarget.msslab.redlabnet.com"
+
+
+const timeoutMillisecs = 0
+
+let maxBadTriesBeforeGoodTry = 40
+let maxGoodTriesBeforeBadTry = 3
+const maxTries = 1000
+
+
+let token = ''
+let count = 0
+let isBadTry = true
+let firstBadTry = 0
+let firstGoodTry = 0
+const username = 'user'
+const password = 'pass'
+
+function pickEvilRandomNumber() {
+    return Math.floor(Math.random() * 9) + 1;
+}
+
+function pickNormalRandomNumber() {
+    return Math.floor(Math.random() * 1000) + 10; // Ensure we never return < 11
+}
+
+//TODO: Replace all the commented out setTimeout() functions with delay()
+function delay(t, val) {
+    return new Promise(resolve => setTimeout(resolve, t, val));
+}
+const login = async () => {
+        const loginApiUrl = baseUrl + '/api/login'
+        // Determine whether we use a valid user or an invalid user
+        if ((isBadTry && (count - firstBadTry) >= maxBadTriesBeforeGoodTry)) {
+            isBadTry = false
+            firstGoodTry = count
+        } else if ((!isBadTry && (count - firstGoodTry) >= maxGoodTriesBeforeBadTry)) {
+            isBadTry = true
+            firstBadTry = count
+        }
+       
+        const randoId = isBadTry ? pickNormalRandomNumber() : pickEvilRandomNumber()
+
+        const user = username + randoId
+        const pass = password + randoId
+
+        await delay(timeoutMillisecs) 
+
+        const res = await fetch(loginApiUrl, {
+            method: 'POST',
+            redirect: 'manual',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+
+            body: `username=${encodeURIComponent(user)}&password=${encodeURIComponent(pass)}`
+        });
+
+        console.log(`/api/login returned ${res.status} for user ${user} and flag ${isBadTry}`)
+        return res
+}
+
+
+const main = async () => {
+    while (count < maxTries) {
+        const response = await login()
+        // console.log(count)
+        count += 1
+    }
+}
+
+main()
